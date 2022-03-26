5ae696efa212d56fd6a5ca287867b882d6681cb3
Mazan-ka <Mokeevskoe1>
2022-03-26 20:30:29 +0300

    добавил нормальное считывание последней лексемы и конца проги

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -3,30 +3,32 @@ import java.util.*;
 
 public class Lexer {
     static Map<String, Pattern> lexems = new HashMap<>();
+    static List<Token> tokens = new LinkedList<>();
 
     static { //определение регулярных выражений для лексем
         lexems.put("OP", Pattern.compile("^\\+|-|/|\\*$"));
         lexems.put("VAR", Pattern.compile("^[a-z][a-z0-9]*$"));
-        //lexems.put("KEYWORDS", Pattern.compile("if|else|for"));
+        lexems.put("WHILE_KEYWORD", Pattern.compile("^while$"));
+        lexems.put("FOR_KEYWORD", Pattern.compile("^for$"));
+        lexems.put("IF_KEYWORD", Pattern.compile("^if$"));
+        lexems.put("ELSE_KEYWORD", Pattern.compile("^else$"));
         lexems.put("ASSIGN_OP", Pattern.compile("^=$"));
         lexems.put("DIGIT", Pattern.compile("^0|([1-9][0-9]*)$"));
         lexems.put("R_BRACKET", Pattern.compile("^\\)$"));
         lexems.put("L_BRACKET", Pattern.compile("^\\($"));
     }
 
-    static List<Token> tokens = new LinkedList<>();
-
     public static void main(String[] args) {
-        String src = "num=((100+1)-(50-22))"; //основная строка
+        String src = "num = ((100+1) - (50-22)) + 90; sum = 100/50;"; //основная строка
 
-        match(src);//вызов основного метода лексера
+        lexer(src); //вызов основного метода лексера
 
         System.out.println(src);
         System.out.println("Tokens: ");
         for (Token t : tokens) { System.out.println(t); }
     }
 
-    static void match(String src) {
+    static void lexer(String src) {
         StringBuffer buffer = new StringBuffer("");
         String buffString = "";
         boolean isValid;
@@ -36,8 +38,17 @@ public class Lexer {
         Matcher m;
 
         while (i < length) { //Проходимся по всем символам в строке src
+            if (i == length - 1 & chArray[i] == ';') break; //Условие конца программы
+
             buffer.append(chArray[i]);
-            //System.out.println(buffer);
+
+            if (chArray[i] == ' ' || chArray[i] == ';') { //Пропуск пробелов и точек с запятой в строке
+                buffer.reverse();
+                buffer.deleteCharAt(0);
+                buffer.reverse();
+                i++;
+                continue;
+            }
 
             isValid = false;
 
@@ -46,10 +57,8 @@ public class Lexer {
                 m = p.matcher(buffer);
 
                 if (m.matches()) { //проверка буфера на соответствие регулярным выражениям
-                    //System.out.println("проверка связи!");
-
                     isValid = true;
-                    if (!buffString.equals(lexemName)) buffString = lexemName;
+                    buffString = lexemName;
                 }
             }
 
@@ -58,10 +67,14 @@ public class Lexer {
                 return;
             }
 
-            if (i == length - 1 & buffer.length() == 1) tokens.add(new Token(buffString, buffer)); //условие конечного символа
+            if (chArray[i+1] == ';') { //Условие конечной лексемы в строке
+                tokens.add(new Token(buffString, buffer));
+                buffer.setLength(0);
+                i++;
+                continue;
+            }
 
             if (!isValid) { //проверка конца лексемы в строке
-
                 buffer.reverse();
                 buffer.deleteCharAt(0);
                 buffer.reverse();
@@ -72,21 +85,6 @@ public class Lexer {
             }
 
             i++;
-        }
-    }
-
-
-    static class Token {
-        private final String type;
-        private final StringBuffer value = new StringBuffer("");
-
-        public Token(String type, StringBuffer value) {
-            this.type = type;
-            this.value .append(value);
-        }
-
-        public String toString() {
-            return "Token " + this.type + " : " + this.value + " ";
         }
     }
 }
