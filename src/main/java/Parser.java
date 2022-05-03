253b3194462ccd0ada4d96c7de4a0b1838cc5c55
Mazanka <Stas123564@gmail.com>
2022-05-03 19:26:49 +0300

    Lexer 3.0 пока только assign добавил коменты

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -14,78 +14,43 @@ public class Parser {
 
     public Parser(List<Token> tokens) { // конструктор парсера, принимающий лист с токенами от лексера
         this.tokens = tokens;
-        iterator = tokens.listIterator();
+        iterator = tokens.listIterator(); //присваиваем первому итератору лист с токенами
     }
 
     public void parser() {
-        it = tokens.listIterator();
+        it = tokens.listIterator(); //присваиваем второму итератору лист с токенами
         while (iterator.hasNext()) {
-            expr();
-        }
-
-        /*int temp;
-        while(i <= length - 1) {
-            temp = i;
             try {
-                if (expr())
-            } catch (SyntaxException1 e) {
-                System.out.println("\nСинтаксическая ошибка!");
-                System.out.println("-- " + e.getExplanation());
-                if (i == 0 || i == temp) return;
+                expr(); //вызов основного нетерминала expr
+            } catch (ParseException pe) {
+                System.out.println(pe.getMessage() + pe.getErrorOffset());
+                return;
             }
-        }*/
+        }
     }
 
 
 // _____ПРОВЕРКА_НЕТЕРМИНАЛОВ____________________________________________________
 
-    void expr() {
-        try {
-            if (assign() /*|| while_()*/) {
-                StringBuffer buffer = new StringBuffer();
-                prev();
+    void expr() throws ParseException {
+        if (assign() /*|| while_()*/) {
+            StringBuffer buffer = new StringBuffer();
+            prev(); //двигаем назад основной итератор, для того чтобы конец expression был верный
 
-                while (true) { //добавление полученного выражения в list
-                    buffer.append(it.next().getValue());
-                    it.previous();
-                    if (it.next().getValue().toString().equals(currenToken.getValue().toString())) {
-                        next();
-                        break;
-                    }
+            while (true) { //добавление полученного выражения в list
+                buffer.append(it.next().getValue());
+                it.previous(); //двигаем назад второй итератор, для того чтобы начало следующего expression был верный
+                if (it.next().getValue().toString().equals(currenToken.getValue().toString())) { //если два итератора оказались равны, то прекращаем шагать и записываем всё в новый лист
+                    next(); //возвращаем на место основной итератор
+                    break;
                 }
-                expressions.add(new Expression(buffer));
-            } else {
-                throw new ParseException("ASSIGN or WHILE expected, but: ", tokens.listIterator().nextIndex());
             }
-        } catch (ParseException pe) {
-            System.out.println(pe.getMessage() + ", " + pe.getErrorOffset());
+            expressions.add(new Expression(buffer));
+        } else {
+            throw new ParseException("ASSIGN or WHILE expected, but ", iterator.nextIndex());
         }
-
 
 //        } else if (while_keyword()) { //нетерминал while
-
-
-        /*start = i;
-        if (assign()) { //проверка на терминал присваивания
-            StringBuffer buffer = new StringBuffer(); //добавление полученного выражения в list
-
-            for (int j = start; j <= i - 1; j++) {
-                buffer.append(tokens.get(j).getValue());
-            }
-            expressions.add(new Expression(buffer));
-            return true;
-        }
-
-        if (while_keyword(i) && condition_while() && body_while()) { //проверка на while терминал
-            StringBuffer buffer = new StringBuffer(); //добавление полученного выражения в list
-
-            for (int j = start; j <= i - 1; j++) {
-                buffer.append(tokens.get(j).getValue());
-            }
-            expressions.add(new Expression(buffer));
-            return true;
-        }
-        return false;*/
     }
 
     protected boolean assign() throws ParseException { //основной терминал assign
@@ -93,30 +58,18 @@ public class Parser {
             if (assign_op()) {
                 return expr_value();
             } else {
-                throw new ParseException("ASSIGN_OP expected, but", tokens.listIterator().nextIndex());
+                throw new ParseException("ASSIGN_OP expected, but", iterator.nextIndex());
             }
         } return false;
     }
 
     protected boolean expr_value() throws ParseException {
         if (value()) {
-            if (!tokens.listIterator().hasNext()) return true;
+            if (!iterator.hasNext()) return true;
             else return op_value();
         } else {
-            throw new ParseException("VAR or DIGIT expected, but", tokens.listIterator().nextIndex());
+            throw new ParseException("VAR or DIGIT expected, but", iterator.nextIndex());
         }
-
-        /*if (i == tokens.size() - 1 && value()) return true; //случай последнего op_value и конца токенов типа a=100
-        int temp = i;
-        if (value() && op_v2(i)) { //случай конца нетерминала assign, но не конца токенов типа a=100
-            return true;
-        } else if (temp != i) i = temp;
-        if (value()) {
-            try {
-                op_value();
-            } catch (SyntaxException2 ignored) {}
-        }
-        return true;*/
     }
 
     protected boolean op_value() throws ParseException {
@@ -124,39 +77,14 @@ public class Parser {
             if (value()) {
                 if (iterator.hasNext() && op()) {
                     prev();
-                    return op_value();
+                    return op_value(); //рекурсивный вызов функции для случаев с большим количеством операций
                 } return true;
             } else {
-                throw new ParseException("VAR or DIGIT expected, but", tokens.listIterator().nextIndex());
+                throw new ParseException("VAR or DIGIT expected, but", iterator.nextIndex());
             }
         } else {
-            throw new ParseException("OP expected, but", tokens.listIterator().nextIndex());
+            throw new ParseException("OP expected, but", iterator.nextIndex());
         }
-
-        //value(i + 1); throw new SyntaxException1("Не хватает лексем/терминалов - " + i);
-
-        //проверка ошибки в коде вида: sum = 100 +;
-        /*int temp = i;
-        if (i == tokens.size() - 1 && op(i))  throw new SyntaxException1("Не хватает лексем/терминалов - " + i);
-        else if (temp != i) i--;
-
-        try {
-            if (i + 1 == tokens.size() - 1 && op(i) && value()) { //случай последнего op_value и конца токенов
-                return;
-            }
-
-            temp = i;
-            if (op(i) && value() && op_v2(i)) { //случай конца нетерминала assign, но не конца токенов
-                return;
-            } else if (temp != i) i = temp;
-
-            if (i + 1 < tokens.size() - 1 && op(i) && value()) { //случай продолжения токенов
-                op_value();
-            }
-        } catch (SyntaxException1 e) { //обработка положительного исключения
-            Parser.i--;
-            throw new SyntaxException2("");
-        }*/
     }
 
     protected boolean value() {
@@ -198,19 +126,6 @@ public class Parser {
 
 
 //  _____ПРОВЕРКА_ЛЕКСЕМ/ТЕРМИНАЛОВ________________________________________________________
-
-    // старый метод для терминалов
-/*    static boolean var(int i) throws SyntaxException1 {
-        Pattern p = Lexer.lexems.get("VAR");
-        Matcher m = p.matcher(tokens.get(i).getValue()); //создание matcher с нужной регуляркой и загрузка в него токена
-
-        //создание исключения в случае несовпадения лексем или возврат true
-        if (!m.matches()) throw new SyntaxException1("Error: token number - " + i);
-        else {
-            Parser.i++;
-            return true;
-        }
-    }*/
 
     protected boolean var() {
         return checkToken("VAR");
@@ -257,17 +172,17 @@ public class Parser {
     }
 
     protected void next() {
-        this.currenToken = iterator.next();
+        this.currenToken = iterator.next(); //двигаем итератор по листу токенов на один элемент вперед
     }
 
     protected void prev() {
-        this.currenToken = iterator.previous();
+        this.currenToken = iterator.previous(); //двигаем итератор по листу токенов на один элемент назад
     }
 
     protected boolean checkToken(String name) {
         next();
-        boolean result = this.currenToken.getType().equals(name);
-        if (!result) {
+        boolean result = this.currenToken.getType().equals(name); //проверяем, равен ли текущий токен тому, что передали в переменную name
+        if (!result) { //если токен не совпал, то двигаем итератор назад
             prev();
         }
         return result;
