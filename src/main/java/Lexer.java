5a37e65a967a8d7869652ea6feb06d4c25168272
Mazanka <Stas123564@gmail.com>
2022-05-21 02:55:23 +0300

    Изменил комментарии и поправил код

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -6,29 +6,29 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class Lexer {
-    static Map<String, Pattern> lexems = new HashMap<>();
-    static Map<String, Pattern> var_lexems = new HashMap<>();
-    static List<Token> tokens = new LinkedList<>();
+    private final static Map<String, Pattern> LEXEMS = new HashMap<>();
+    private final static Map<String, Pattern> VAR_LEXEMS = new HashMap<>();
+    private final static List<Token> tokens = new LinkedList<>();
 
     static { //определение регулярных выражений для лексем
-        lexems.put("OP", Pattern.compile("^\\+|-|/|\\*$"));
-        lexems.put("OP_BOOL", Pattern.compile("^>|<|==|!|!=$"));
-        lexems.put("ASSIGN_OP", Pattern.compile("^=$"));
-        lexems.put("DIGIT", Pattern.compile("^0|([1-9][0-9]*)$"));
+        LEXEMS.put("OP", Pattern.compile("^\\+|-|/|\\*$"));
+        LEXEMS.put("OP_BOOL", Pattern.compile("^>|<|==|!|!=$"));
+        LEXEMS.put("ASSIGN_OP", Pattern.compile("^=$"));
+        LEXEMS.put("DIGIT", Pattern.compile("^0|([1-9][0-9]*)$"));
 
-        lexems.put("R_BRACKET", Pattern.compile("^\\)$"));
-        lexems.put("L_BRACKET", Pattern.compile("^\\($"));
-        lexems.put("START_BODY", Pattern.compile("^\\{$"));
-        lexems.put("FINISH_BODY", Pattern.compile("^}$"));
-        lexems.put("COMMENTS", Pattern.compile("^#$"));
+        LEXEMS.put("R_BRACKET", Pattern.compile("^\\)$"));
+        LEXEMS.put("L_BRACKET", Pattern.compile("^\\($"));
+        LEXEMS.put("START_BODY", Pattern.compile("^\\{$"));
+        LEXEMS.put("FINISH_BODY", Pattern.compile("^}$"));
+        LEXEMS.put("COMMENTS", Pattern.compile("^#$"));
 
-        lexems.put("VAR", Pattern.compile("^[a-z][a-z0-9]*$"));
+        LEXEMS.put("VAR", Pattern.compile("^[a-z][a-z0-9]*$"));
 
-        var_lexems.put("WHILE_KEYWORD", Pattern.compile("^while$"));
-        var_lexems.put("FOR_KEYWORD", Pattern.compile("^for$"));
-        var_lexems.put("IF_KEYWORD", Pattern.compile("^if$"));
-        var_lexems.put("ELSE_KEYWORD", Pattern.compile("^else$"));
-        var_lexems.put("BOOL_KEYWORD", Pattern.compile("^true|false$"));
+        VAR_LEXEMS.put("WHILE_KEYWORD", Pattern.compile("^while$"));
+        VAR_LEXEMS.put("FOR_KEYWORD", Pattern.compile("^for$"));
+        VAR_LEXEMS.put("IF_KEYWORD", Pattern.compile("^if$"));
+        VAR_LEXEMS.put("ELSE_KEYWORD", Pattern.compile("^else$"));
+        VAR_LEXEMS.put("BOOL_KEYWORD", Pattern.compile("^true|false$"));
     }
 
     //главная main функция
@@ -39,7 +39,9 @@ public class Lexer {
 
             System.out.println(l.Reader());
             System.out.println("Tokens: ");
-            for (Token t : tokens) { System.out.println(t); }
+            for (Token t : tokens) {
+                System.out.println(t);
+            }
         } catch (GrammarException e) {
             System.out.println("Грамматическая ошибка!");
         }
@@ -48,7 +50,7 @@ public class Lexer {
         Parser p = new Parser(tokens);
         p.parser();
         System.out.print("\n");
-        for (Expression s: Parser.expressions) {
+        for (Expression s : Parser.expressions) {
             System.out.println(s);
         }
     }
@@ -58,12 +60,12 @@ public class Lexer {
         try {
             File file = new File("write_your_code_here.txt"); //создание объекта с файлом
 
-            if (!file.exists()) file.createNewFile(); //создание файла, если его нет
-
             BufferedReader bf = new BufferedReader(new FileReader(file)); //открытие потока BufferReader, с помощью которого будет считываться файл
-            String buffer = "";
+            String buffer;
 
             while ((buffer = bf.readLine()) != null) { //считывание одной строки с файла и занесение её в буфер
+                if (buffer.isEmpty()) continue; //проверка строки на пустоту, к примеру просто перенос строки
+                if (buffer.trim().charAt(0) == '/' && buffer.trim().charAt(1) == '/') continue; //пропускаем строку, так как она является комментарием
                 src = src.concat(buffer); //склеивание основной строки с буфером
             }
 
@@ -72,28 +74,23 @@ public class Lexer {
             System.out.println("File creating or opening error! " + e);
         }
 
-        if (src.equals(";") || src.equals("") || src.equals(" ")) { //проверка корректности строки
-            throw new GrammarException(); //создание исключения об ошибке синтаксиса
-        }
-
         return src;
     }
 
     protected void lexer(String src) {
-        StringBuffer buffer = new StringBuffer("");
+        StringBuffer buffer = new StringBuffer();
         String buffString = "";
         boolean isValid;
-        char[] chArray = src.toCharArray();
         int i = 0;
-        int length = chArray.length;
+        int length = src.length();
         Matcher m;
 
         while (i < length) { //Проходимся по всем символам в строке src
-            if (i == length - 1 && chArray[i] == ';') break; //Условие конца программы
+            if (i == length - 1 && src.charAt(i) == ';' || src.charAt(i) == '\t') break; //Условие конца программы
 
-            buffer.append(chArray[i]);
+            buffer.append(src.charAt(i));
 
-            if (chArray[i] == ' ' || chArray[i] == ';') { //Пропуск пробелов или точек с запятой в строке
+            if (src.charAt(i) == ' ' || src.charAt(i) == ';') { //Пропуск пробелов или точек с запятой в строке
                 buffer.reverse();
                 buffer.deleteCharAt(0);
                 buffer.reverse();
@@ -103,16 +100,16 @@ public class Lexer {
 
             isValid = false;
 
-            for (String lexemName : lexems.keySet()) {
-                Pattern p = lexems.get(lexemName);
-                Pattern p_sec = lexems.get("VAR"); //специальный паттерн для var лексем
+            for (String lexemName : LEXEMS.keySet()) {
+                Pattern p = LEXEMS.get(lexemName);
+                Pattern p_sec = LEXEMS.get("VAR"); //специальный паттерн для var лексем
                 m = p_sec.matcher(buffer);
 
                 if (m.matches()) { //проверка для отделения ключевых слов от названий переменных
                     boolean isValid_var = false;
 
-                    for (String lexemName_var : var_lexems.keySet()) {
-                        Pattern p_var = var_lexems.get(lexemName_var);
+                    for (String lexemName_var : VAR_LEXEMS.keySet()) {
+                        Pattern p_var = VAR_LEXEMS.get(lexemName_var);
                         m = p_var.matcher(buffer);
 
                         if (m.matches()) { //проверка буфера на соответствие регулярным выражениям
@@ -143,31 +140,23 @@ public class Lexer {
                 buffer.deleteCharAt(0);
                 buffer.reverse();
 
-                if (buffString.equals("COMMENTS")) { //условие для нахождения комментария в коде
-                    i--;
-                    while (chArray[i] != ';') i++;
-
-                    buffer.setLength(0);
-                    continue;
-                }
-
                 tokens.add(new Token(buffString, buffer));
                 buffer.setLength(0);
                 continue;
             }
 
-            if (chArray[i+1] == ';') { //условие конечной лексемы в строке
+            if (src.charAt(i + 1) == ';') { //условие конечной лексемы в строке
                 tokens.add(new Token(buffString, buffer));
                 buffer.setLength(0);
                 i++;
                 continue;
             }
-
             i++;
         }
     }
 }
 
-class GrammarException extends Exception{}
+class GrammarException extends Exception {
+}
 
 
